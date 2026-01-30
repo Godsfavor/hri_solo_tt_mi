9f59c2bdaed9e352aed8f540289d47b4d90de3c8
Godsfavour <Godsfavour.gabriel@gmail.com>
2026-01-30 17:55:15 +0100

    Teleop_ui and wasd working...

--- Assets/Scripts/BlueRovVeloControl.cs
+++ Assets/Scripts/BlueRovVeloControl.cs
@@ -5,30 +5,47 @@ public class BlueRovVeloControl : MonoBehaviour {
     public float lvx = 0.0f; 
     public float lvy = 0.0f;
     public float lvz = 0.0f;
-    public float avz = 0.0f;
+    public float avx = 0.0f;  // Roll
+    public float avy = 0.0f;  // Pitch
+    public float avz = 0.0f;  // Yaw
     public bool movementActive = false;
     public Rigidbody rb;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         this.rb = GetComponent<Rigidbody>();
     }
     
     private void moveVelocityRigidbody() {
-        Vector3 movement = new Vector3(-lvx * Time.deltaTime, lvz * Time.deltaTime, lvy * Time.deltaTime);
-        transform.Translate(movement);
-        transform.Rotate(0, avz * Time.deltaTime,0);
+        // Translation: ROS to Unity coordinate mapping
+        Vector3 movement = new Vector3(-lvx * Time.fixedDeltaTime, 
+                                       -lvz * Time.fixedDeltaTime,  
+                                       lvy * Time.fixedDeltaTime);
+        transform.Translate(movement, Space.Self);
+        
+        // Rotation: Apply roll, pitch, and yaw
+        // ROS uses radians, Unity uses degrees
+        float rollDeg = avx * Time.fixedDeltaTime * Mathf.Rad2Deg;
+        float pitchDeg = avy * Time.fixedDeltaTime * Mathf.Rad2Deg;
+        float yawDeg = avz * Time.fixedDeltaTime * Mathf.Rad2Deg;
+        
+        // Apply rotations in local space
+        transform.Rotate(rollDeg, yawDeg, pitchDeg, Space.Self);
     }
     
     public void moveVelocity(RosMessageTypes.Geometry.TwistMsg velocityMessage) {
+        // Linear velocities
         this.lvx = (float)velocityMessage.linear.x;
         this.lvy = (float)velocityMessage.linear.y;
         this.lvz = (float)velocityMessage.linear.z;
-        this.avz = (float)velocityMessage.angular.z;
+        
+        // Angular velocities
+        this.avx = (float)velocityMessage.angular.x;  // Roll
+        this.avy = (float)velocityMessage.angular.y;  // Pitch
+        this.avz = (float)velocityMessage.angular.z;  // Yaw
+        
         this.movementActive = true;
     }
 
-    // Update is called once per frame
     void FixedUpdate() {
         if (movementActive) {
             moveVelocityRigidbody();
--- Assets/Scripts/KeyboardCmdVelPublisher.cs
+++ Assets/Scripts/KeyboardCmdVelPublisher.cs
@@ -1,58 +1,122 @@
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
-using RosMessageTypes.Geometry;  // For TwistMsg
+using RosMessageTypes.Geometry;
 
 public class KeyboardCmdVelPublisher : MonoBehaviour
 {
     public string topic = "/bluerov1/cmd_vel";
-    public float forwardSpeed = 0.5f;  // tune to your liking
-    public float yawSpeed = 0.5f;      // optional (A/D turn)
+    public float forwardSpeed = 0.5f;      // X-axis (forward/backward)
+    public float strafeSpeed = 0.5f;       // Y-axis (left/right strafe)
+    public float verticalSpeed = 0.5f;     // Z-axis (up/down)
+    public float rollSpeed = 0.5f;         // Roll (rotation around X-axis)
+    public float pitchSpeed = 0.5f;        // Pitch (rotation around Y-axis)
+    public float yawSpeed = 0.5f;          // Yaw (rotation around Z-axis)
+    
+    [Header("Control Settings")]
+    [Tooltip("Only publish commands when Unity window has focus")]
+    public bool onlyWhenFocused = true;
 
     private ROSConnection ros;
 
     void Start()
     {
-        // Get or create the ROS connection and register as a publisher
         ros = ROSConnection.GetOrCreateInstance();
         ros.RegisterPublisher<TwistMsg>(topic);
     }
 
     void Update()
     {
-        // We will send cmd_vel while keys are pressed
-        var twist = new TwistMsg();
-        bool send = false;
+        // Only process input if Unity window has focus (or if onlyWhenFocused is disabled)
+        if (onlyWhenFocused && !Application.isFocused)
+        {
+            return; // Don't publish anything when Unity doesn't have focus
+        }
 
-        // Exercise requirement: move when W is pressed
+        var twist = new TwistMsg();
+        bool anyKeyPressed = false;
+
+        // ===== TRANSLATIONS =====
+        
+        // W/S keys - Left/Right translation
         if (Input.GetKey(KeyCode.W))
         {
-            twist.linear.z = forwardSpeed;
-            send = true;
+            twist.linear.x = forwardSpeed;      // Left translation
+            anyKeyPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            twist.linear.x = -forwardSpeed;     // Right translation
+            anyKeyPressed = true;
         }
 
-        // Extra (optional): S backwards, A/D yaw
-        if (Input.GetKey(KeyCode.S))
-        {
-            twist.linear.z = -forwardSpeed;
-            send = true;
-        }
+        // A/D keys - Back/Forward translation
         if (Input.GetKey(KeyCode.A))
         {
-            twist.angular.z = yawSpeed;
-            send = true;
+            twist.linear.y = strafeSpeed;       // Back translation
+            anyKeyPressed = true;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            twist.angular.z = -yawSpeed;
-            send = true;
+            twist.linear.y = -strafeSpeed;      // Forward translation
+            anyKeyPressed = true;
         }
 
-        // Only publish if any key is pressed
-        if (send)
+        // Q/E keys - Up/Down translation
+        if (Input.GetKey(KeyCode.Q))
+        {
+            twist.linear.z = verticalSpeed;     // Upward translation
+            anyKeyPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            twist.linear.z = -verticalSpeed;    // Downward translation
+            anyKeyPressed = true;
+        }
+
+        // ===== ROTATIONS =====
+        
+        // R/F keys - Pitch (nose up/down)
+        if (Input.GetKey(KeyCode.R))
+        {
+            twist.angular.x = rollSpeed;        // Downward pitch (nose of BlueRov faces down)
+            anyKeyPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            twist.angular.x = -rollSpeed;       // Upward pitch (nose of BlueRov faces up)
+            anyKeyPressed = true;
+        }
+
+        // T/G keys - Roll (side tilt)
+        if (Input.GetKey(KeyCode.T))
+        {
+            twist.angular.y = pitchSpeed;       // Roll (left side of BlueRov2 rises/faces upward)
+            anyKeyPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            twist.angular.y = -pitchSpeed;      // Roll (right side of BlueRov2 rises/faces upward)
+            anyKeyPressed = true;
+        }
+
+        // Z/C keys - Yaw (turn left/right)
+        if (Input.GetKey(KeyCode.Z))
+        {
+            twist.angular.z = yawSpeed;         // Yaw (front of BlueRov2 turns left)
+            anyKeyPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            twist.angular.z = -yawSpeed;        // Yaw (front of BlueRov2 turns right)
+            anyKeyPressed = true;
+        }
+
+        // Only publish if Unity has focus and a key is pressed
+        // This prevents sending zero commands when switching to terminal
+        if (anyKeyPressed)
         {
             ros.Publish(topic, twist);
+            Debug.Log($"Unity Publishing: linear({twist.linear.x:F2}, {twist.linear.y:F2}, {twist.linear.z:F2}) angular({twist.angular.x:F2}, {twist.angular.y:F2}, {twist.angular.z:F2})");
         }
     }
 }
-
-
